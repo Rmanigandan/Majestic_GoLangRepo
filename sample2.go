3ec555b21d59a98935e709fba6d82b7aa770da4b
Rmanigandan <65057700+Rmanigandan@users.noreply.github.com>
2022-10-27 14:47:05 +0530

    Fourth commit

--- sample2.go
+++ sample2.go
@@ -6,4 +6,5 @@ func main() {
 	fmt.Println("Hello Developers Iam Golang Repos By Majestic sample2")
 	//here changes done
 	//here double changes gone
+	//I have done the chaneges int the global repository
 }
