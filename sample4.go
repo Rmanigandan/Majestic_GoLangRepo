de19a379e33d8ae54267d706c9567c7bdcb420c1
manikandan.r <manikandan.r@vembu.com>
2022-10-27 02:38:11 -0700

    Four files

---
+++ sample4.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello Developers Iam Golang Repos By Majestic sample2")
+	//Iam the new file created in the local environment sample4.go
+}
